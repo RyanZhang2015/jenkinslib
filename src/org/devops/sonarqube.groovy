882a11ed75f5c2deb9ec4616d144dd649775bf9b
RyanZhang2015 <181079990@qq.com>
2020-03-22 19:06:45 +0800

    update

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -3,7 +3,7 @@ package org.devops
 //scan
 def SonarScan(projectName,projectDesc,projectPath){
     withSonarQubeEnv("sonar-server"){
-        scannerHome = tool "sonarscan"
+        def scannerHome = tool "sonarscan"
         def sonarDate = sh returnStdout: true, script: 'date  +%Y%m%d%H%M%S'
         sonarDate = sonarDate - "\n"
         sh """ 
