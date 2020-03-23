fd171f77035501e3d86d7c514d1bdabe647afa66
RyanZhang2015 <181079990@qq.com>
2020-03-23 20:30:53 +0800

    update

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -11,7 +11,7 @@ def SonarScan(projectName,projectDesc,projectPath){
         -Dsonar.projectName=${projectName} -Dsonar.projectVersion=${sonarDate} -Dsonar.ws.timeout=30 \
         -Dsonar.projectDescription=${projectDesc} -Dsonar.links.homepage=http://www.baidu.com \
         -Dsonar.sources=${projectPath} -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=target/classes \
-        -Dsonar.java.test.binaries=target/test-classes -Dsonar.java.surefire.report=target/surefire-reports  -Dsonar.branch.name=${branchName} -X
+        -Dsonar.java.test.binaries=target/test-classes -Dsonar.java.surefire.report=target/surefire-reports
         """
     }
 }
