6133a9686c0d3e55f7c571a518a3cb045c9616a9
RyanZhang2015 <181079990@qq.com>
2020-04-01 23:36:28 +0800

    add artifact

--- src/org/devops/nexus.groovy
+++ src/org/devops/nexus.groovy
@@ -43,10 +43,47 @@ def MavenUpload(){
         ${mvnHome}/bin/mvn deploy:deploy-file -Dmaven.test.skip=true  \
                                 -Dfile=${jarName} -DgroupId=${pomGroupId} \
                                 -DartifactId=${pomArtifact} -Dversion=${pomVersion}  \
-                                -Dpackaging=${pomPackaging} -DrepositoryId=maven-hostd \
-                                -Durl=http://192.168.3.41:30010/repository/maven-hostd 
+                                -Dpackaging=${pomPackaging} -DrepositoryId=maven-snapshots \
+                                -Durl=http://192.168.3.41:30010/repository/maven-snapshots 
         """
 }
+
+//制品晋级
+def ArtifactUpdate(updateType,artifactUrl){
+
+    //晋级策略
+    if ("${updateType}" == "snapshot -> release"){
+        println("snapshot -> release")
+
+        //下载原始制品
+        sh "  rm -fr updates && mkdir updates && cd updates && wget ${artifactUrl} && ls -l "
+
+        //获取artifactID 
+        
+        artifactUrl = artifactUrl -  "http://192.168.3.41:30010/repository/maven-snapshots/"
+        artifactUrl = artifactUrl.split("/").toList()
+        
+        println(artifactUrl.size())
+        env.jarName = artifactUrl[-1] 
+        env.pomVersion = artifactUrl[-2].replace("SNAPSHOT","RELEASE")
+        env.pomArtifact = artifactUrl[-3]
+        pomPackaging = artifactUrl[-1]
+        pomPackaging = pomPackaging.split("\\.").toList()[-1]
+        env.pomPackaging = pomPackaging[-1]
+        env.pomGroupId = artifactUrl[0..-4].join(".")
+        println("${pomGroupId}##${pomArtifact}##${pomVersion}##${pomPackaging}")
+        env.newJarName = "${pomArtifact}-${pomVersion}.${pomPackaging}"
+        
+        //更改名称
+        sh " cd updates && mv ${jarName} ${newJarName} "
+        
+        //上传制品
+        env.repoName = "maven-release"
+        env.filePath = "updates/${newJarName}"
+        NexusUpload()
+    }
+}
+
 
 def main(uploadType){
     GetGav()
